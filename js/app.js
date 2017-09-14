d05d3f324e974780ef77f392ce7e1af491768568
Ivan Limongan <ivan.limongan@gmail.com>
2017-09-13 20:59:32 -0700

    list working

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
 // js
 console.log("start");
 
-var list = function(name, background) {
+var portfolio = function(name, background) {
   this.name = name;
   this.background = background;
 }
 
-var portList = [];
-portList.push(new list("outtpost", "img/op4.jpg"));
-portList.push(new list("outtpost", "img/"));
-portList.push(new list("outtpost", "img/"));
-portList.push(new list("outtpost", "img/"));
+var portfolioList = [];
+portfolioList.push(new portfolio("outtpost", "img/op4.jpg"));
+portfolioList.push(new portfolio("intrinzic", "img/hive.jpg"));
+// portfolioList.push(new portfolio("mpowered media", "img/"));
+// portfolioList.push(new portfolio("outtpost", "img/"));
 
 
+function makeList() {
+  var build = document.getElementById("projectNames");
+  for (index = 0; index < portfolioList.length; index++) {
+    var port = portfolioList[index];
+    var portList = document.createElement("li");
+    portList.innerText = port.name;
+    portList.addEventListener("click", show);
+    portList.dataset.index = index;
+    build.appendChild(portList);
+  }
+}
+
+function show(event) {
+  var click = event.target;
+}
 
 
-
-window.addEventListener("load", portfolioBuild);
+window.addEventListener("load", makeList);
 
 console.log("end");
