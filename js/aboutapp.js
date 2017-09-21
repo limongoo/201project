6ecb1694fa0e5ae4c69006aa5f3fb82119c4f956
Ivan Limongan <ivan.limongan@gmail.com>
2017-09-20 20:32:44 -0700

    add aboutapp js and indexapp js for page transitions, form works on both pages

--- js/aboutapp.js
+++ js/aboutapp.js
@@ -1,50 +1,23 @@
-// js
-console.log("start");
+// page transition
+var link = document.getElementById('aab');
+link.addEventListener('click', function (event) {
+  event.preventDefault();
+  document.body.classList.remove("fadein");
+  document.body.classList.add("fadeout");
+  setTimeout(function(){
+    location.assign(event.target.href);
+  },500);
+})
 
-// constructor
-var portfolio = function(name, background) {
-  this.name = name;
-  this.background = background;
-}
-
-// array
-var portfolioList = [];
-portfolioList.push(new portfolio("outt—post", "op4.png"));
-portfolioList.push(new portfolio("intrinzic", "hive.jpg"));
-// portfolioList.push(new portfolio("chambray", ."jpg"));
-portfolioList.push(new portfolio("mPowered media", "3dhouse.jpg"));
-portfolioList.push(new portfolio("m realty", "mrealty-slogan.jpg"));
-
-// make list
-function makeList() {
-  var build = document.getElementById("projectNames");
-  for (index = 0; index < portfolioList.length; index++) {
-    var port = portfolioList[index];
-    var portList = document.createElement("li");
-    portList.setAttribute("id", "projecth1");
-    portList.dataset.backImg = port.background;
-    portList.innerText = port.name;
-    portList.addEventListener("mouseover", show); // on mouse over trigger function 'show'
-    portList.addEventListener("mouseout", out);
-    build.appendChild(portList);
-  }
-}
-
-// show background function
-function show(event) {
-  var hover = event.target;
-  var back = document.getElementById("showBackground");
-  back.innerHTML = "";
-  var image = document.createElement("img");
-  image.src = "img/" + hover.dataset.backImg
-  back.appendChild(image);
-}
-
-// out background function
-function out() {
-  var back = document.getElementById("showBackground");
-  back.innerHTML = "";
-}
+var link = document.getElementById('bbc');
+link.addEventListener('click', function (event) {
+  event.preventDefault();
+  document.body.classList.remove("fadein");
+  document.body.classList.add("fadeout");
+  setTimeout(function(){
+    location.assign(event.target.href);
+  },500);
+})
 
 // form input
 var submitButton = document.getElementById('submitDiv');
@@ -114,12 +87,3 @@ function closeContactModal() {
   var xContact = document.getElementsByClassName('modal active')[0];
   xContact.setAttribute('class', 'modal');
 }
-
-
-
-
-
-
-window.addEventListener("load", makeList);
-
-console.log("end");
---
+++ js/indexapp.js
@@ -0,0 +1,20 @@
+// page transition
+var link = document.getElementById('aaa');
+link.addEventListener('click', function (event) {
+  event.preventDefault();
+  document.body.classList.remove("fadein");
+  document.body.classList.add("fadeout");
+  setTimeout(function(){
+    location.assign(event.target.href);
+  },500);
+})
+
+var link = document.getElementById('bbb');
+link.addEventListener('click', function (event) {
+  event.preventDefault();
+  document.body.classList.remove("fadein");
+  document.body.classList.add("fadeout");
+  setTimeout(function(){
+    location.assign(event.target.href);
+  },500);
+})
