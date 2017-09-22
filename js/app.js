f09ca7ddc2e30ed2d7b89f6caf1568537a927f5c
ivan limongan <ivan.limongan@gmail.com>
2017-09-22 14:47:31 -0700

    trying to add href to project names, not working yet

--- js/app.js
+++ js/app.js
@@ -2,28 +2,35 @@
 console.log("start");
 
 // constructor
-var portfolio = function(name, background) {
+var portfolio = function(name, background, link) {
   this.name = name;
   this.background = background;
+  this.link = link;
 }
 
 // array
 var portfolioList = [];
-portfolioList.push(new portfolio("outt—post", "op4.png"));
-portfolioList.push(new portfolio("intrinzic", "hive.jpg"));
+portfolioList.push(new portfolio("outt—post", "op4.png", "outtpost.html"));
+portfolioList.push(new portfolio("intrinzic", "hive.jpg", "hive.html"));
 // portfolioList.push(new portfolio("chambray", ."jpg"));
-portfolioList.push(new portfolio("mPowered media", "3dhouse.jpg"));
-portfolioList.push(new portfolio("m realty", "mrealty-slogan.jpg"));
+portfolioList.push(new portfolio("mPowered media", "3dhouse.jpg", "mpowered.html"));
+portfolioList.push(new portfolio("m realty", "mrealty-slogan.jpg", "mrealty.html"));
 
 // make list
 function makeList() {
   var build = document.getElementById("projectNames");
   for (index = 0; index < portfolioList.length; index++) {
     var port = portfolioList[index];
+    var linkName = port.name;
+
     var portList = document.createElement("li");
     portList.setAttribute("id", "projecth1");
     portList.dataset.backImg = port.background;
     portList.innerText = port.name;
+
+    var alink = document.createElement("a").setAttribute("href", port.link);
+    linkName.appendChild(alink);
+
     portList.addEventListener("mouseover", show); // on mouse over trigger function 'show'
     portList.addEventListener("mouseout", out);
     build.appendChild(portList);
