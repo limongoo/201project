98e2bcd8aee35a75f51fac84d2a00a023d6871bd
Ivan Limongan <ivan.limongan@gmail.com>
2017-09-21 18:26:56 -0700

    mood hours all hours 24hr

--- js/mood.js
+++ js/mood.js
@@ -5,10 +5,79 @@ window.onload = (function() {
 function moodTime() {
   var now = new Date();
   var hour = now.getHours();
-  if(hour == 12){
+  if(hour == 0){
     document.getElementById('mood').innerHTML = "Current mood: Snoring";
   }
-  if(hour >= 12 + 8){
+  else if(hour == 1){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 2){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 3){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 4){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 5){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 6){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 7){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 8){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 9){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 10){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 11){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 12){
+    document.getElementById('mood').innerHTML = "Current mood: Snoring";
+  }
+  else if(hour == 13){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 14){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 15){
+    document.getElementById('mood').innerHTML = "Current mood: Need second lunch";
+  }
+  else if(hour == 16){
+    document.getElementById('mood').innerHTML = "Current mood: Tired and hungry";
+  }
+  else if(hour == 17){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 18){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 19){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 20){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 21){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 22){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 23){
+    document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
+  }
+  else if(hour == 24){
     document.getElementById('mood').innerHTML = "Current mood: Eating late night munchies";
   }
   else {
