4d73f3a16aee8ef14c7265d4acb7a6234360c003
ivan limongan <ivan.limongan@gmail.com>
2017-09-28 15:25:19 -0700

    scroll working, not on index

--- js/app.js
+++ js/app.js
@@ -74,7 +74,22 @@ function outTitle() {
 }
 
 // scroll reveal - Scroll Reveal from https://scrollrevealjs.org/
-window.sr = ScrollReveal({origin: 'bottom'});
+function addImgClass () {
+  var pi = document.getElementById('projectContain');
+  var pimg = document.getElementsByTagName('IMG')
+    for (i=0; i <pimg.length; i++) {
+      pimg[i].setAttribute('class', 'sr');
+    }
+}
+addImgClass();
+
+window.sr = ScrollReveal({
+  origin: 'bottom',
+  duration: 1000,
+  opacity: 0,
+  useDelay: 'always',
+  reset: false,
+});
 var revealContainer = document.getElementById('projectContain');
 sr.reveal('.sr', {container: projectContain});
 
